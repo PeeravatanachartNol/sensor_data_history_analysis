f0b66bddd159c9b2435a600421f650aa3fd33e5d
PeeravatanachartNol <peeravatanachartnol@firstloop-tech.com>
2024-11-18 18:54:44 +0900

    Enable write without making an index column

--- split_csv.py
+++ split_csv.py
@@ -9,7 +9,7 @@ df_11 = df[df["sensor_id"].isin(bit_to_sid_11.values())]
 df_11 = df_11.sort_values(by="timestamp")
 
 # write
-df_10.to_csv("./data/4f_sample_10.csv")
-df_11.to_csv("./data/4f_sample_11.csv")
+df_10.to_csv("./data/4f_sample_10.csv", index=False)
+df_11.to_csv("./data/4f_sample_11.csv", index=False)
 
 print("Split csv into two csv's for STV 10 and STV 11")
