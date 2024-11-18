ddb11558c7095a0c84ebaba30fa6933791e919a8
PeeravatanachartNol <peeravatanachartnol@firstloop-tech.com>
2024-11-18 16:23:40 +0900

    Split into two files and order by timestamp

--- split_csv.py
+++ split_csv.py
@@ -3,13 +3,13 @@ from dicts import *
 
 # read and split df into 10 and 11
 df = pd.read_csv("./data/4f_sample.csv", encoding="ISO-8859-1")
-df_10 = df[df["sensor_id"].isin(bit_to_sensor_id_10.values())]
-df_10 = df_10.sort_values(by="id")
-df_11 = df[df["sensor_id"].isin(pos_to_sensor_id_11.values())]
-df_11 = df_11.sort_values(by="id")
+df_10 = df[df["sensor_id"].isin(bit_to_sid_10.values())]
+df_10 = df_10.sort_values(by="timestamp")
+df_11 = df[df["sensor_id"].isin(bit_to_sid_11.values())]
+df_11 = df_11.sort_values(by="timestamp")
 
 # write
-# df_10.to_csv("./data/4f_sample_10.csv")
+df_10.to_csv("./data/4f_sample_10.csv")
 df_11.to_csv("./data/4f_sample_11.csv")
 
 print("Split csv into two csv's for STV 10 and STV 11")
