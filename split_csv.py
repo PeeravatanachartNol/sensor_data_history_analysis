6b3eed0e1baa56336b953e4c3363f8480e6bfea4
PeeravatanachartNol <peeravatanachartnol@firstloop-tech.com>
2024-11-21 14:33:42 +0900

    Add some comments about potentially adding a original_order column

--- split_csv.py
+++ split_csv.py
@@ -8,6 +8,10 @@ df_10 = df_10.sort_values(by=["timestamp", "sensor_id"])
 df_11 = df[df["sensor_id"].isin(bit_to_sid_11.values())]
 df_11 = df_11.sort_values(by=["timestamp", "sensor_id"])
 
+# # insert original_order column
+# df_10.insert(0, "original_order", range(0, len(df_10)))
+# df_11.insert(0, "original_order", range(0, len(df_11)))
+
 # write
 df_10.to_csv("./data/4f_sample_10.csv", index=False)
 df_11.to_csv("./data/4f_sample_11.csv", index=False)
